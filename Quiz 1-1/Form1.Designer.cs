d520e24b318f10df371def8d91a8cd98a93870fd
Robin Joseph <48469274+rj-robinjoseph@users.noreply.github.com>
2023-05-09 22:34:43 -0400

    Add project files.

---
+++ Quiz 1-1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Quiz_1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.books = new System.Windows.Forms.Label();
+            this.authors = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.author1 = new System.Windows.Forms.Label();
+            this.author2 = new System.Windows.Forms.Label();
+            this.author3 = new System.Windows.Forms.Label();
+            this.buttonauthor1 = new System.Windows.Forms.Button();
+            this.buttonauthor2 = new System.Windows.Forms.Button();
+            this.buttonauthor3 = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gone with the Wind";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Roots";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Atlas Shrugged";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // books
+            // 
+            this.books.AutoSize = true;
+            this.books.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.books.Location = new System.Drawing.Point(93, 73);
+            this.books.Name = "books";
+            this.books.Size = new System.Drawing.Size(56, 20);
+            this.books.TabIndex = 3;
+            this.books.Text = "Books";
+            // 
+            // authors
+            // 
+            this.authors.AutoSize = true;
+            this.authors.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authors.Location = new System.Drawing.Point(364, 73);
+            this.authors.Name = "authors";
+            this.authors.Size = new System.Drawing.Size(67, 20);
+            this.authors.TabIndex = 4;
+            this.authors.Text = "Authors";
+            this.authors.Click += new System.EventHandler(this.authors_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(573, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Click button to show Author.";
+            // 
+            // author1
+            // 
+            this.author1.AutoSize = true;
+            this.author1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.author1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.author1.Location = new System.Drawing.Point(328, 129);
+            this.author1.MinimumSize = new System.Drawing.Size(155, 25);
+            this.author1.Name = "author1";
+            this.author1.Size = new System.Drawing.Size(155, 25);
+            this.author1.TabIndex = 6;
+            this.author1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // author2
+            // 
+            this.author2.AutoSize = true;
+            this.author2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.author2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.author2.Location = new System.Drawing.Point(328, 184);
+            this.author2.MinimumSize = new System.Drawing.Size(155, 25);
+            this.author2.Name = "author2";
+            this.author2.Size = new System.Drawing.Size(155, 25);
+            this.author2.TabIndex = 7;
+            this.author2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // author3
+            // 
+            this.author3.AutoSize = true;
+            this.author3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.author3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.author3.Location = new System.Drawing.Point(328, 243);
+            this.author3.MinimumSize = new System.Drawing.Size(155, 25);
+            this.author3.Name = "author3";
+            this.author3.Size = new System.Drawing.Size(155, 25);
+            this.author3.TabIndex = 8;
+            this.author3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonauthor1
+            // 
+            this.buttonauthor1.Location = new System.Drawing.Point(607, 125);
+            this.buttonauthor1.MinimumSize = new System.Drawing.Size(145, 30);
+            this.buttonauthor1.Name = "buttonauthor1";
+            this.buttonauthor1.Size = new System.Drawing.Size(145, 30);
+            this.buttonauthor1.TabIndex = 9;
+            this.buttonauthor1.Text = "Show";
+            this.buttonauthor1.UseVisualStyleBackColor = true;
+            this.buttonauthor1.Click += new System.EventHandler(this.buttonauthor1_Click);
+            // 
+            // buttonauthor2
+            // 
+            this.buttonauthor2.Location = new System.Drawing.Point(607, 180);
+            this.buttonauthor2.MinimumSize = new System.Drawing.Size(145, 30);
+            this.buttonauthor2.Name = "buttonauthor2";
+            this.buttonauthor2.Size = new System.Drawing.Size(145, 30);
+            this.buttonauthor2.TabIndex = 10;
+            this.buttonauthor2.Text = "Show";
+            this.buttonauthor2.UseVisualStyleBackColor = true;
+            this.buttonauthor2.Click += new System.EventHandler(this.buttonauthor2_Click);
+            // 
+            // buttonauthor3
+            // 
+            this.buttonauthor3.Location = new System.Drawing.Point(607, 240);
+            this.buttonauthor3.MinimumSize = new System.Drawing.Size(145, 30);
+            this.buttonauthor3.Name = "buttonauthor3";
+            this.buttonauthor3.Size = new System.Drawing.Size(145, 30);
+            this.buttonauthor3.TabIndex = 11;
+            this.buttonauthor3.Text = "Show";
+            this.buttonauthor3.UseVisualStyleBackColor = true;
+            this.buttonauthor3.Click += new System.EventHandler(this.buttonauthor3_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.buttonClose.Location = new System.Drawing.Point(607, 333);
+            this.buttonClose.MinimumSize = new System.Drawing.Size(145, 30);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(145, 30);
+            this.buttonClose.TabIndex = 12;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonauthor3);
+            this.Controls.Add(this.buttonauthor2);
+            this.Controls.Add(this.buttonauthor1);
+            this.Controls.Add(this.author3);
+            this.Controls.Add(this.author2);
+            this.Controls.Add(this.author1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.authors);
+            this.Controls.Add(this.books);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label books;
+        private System.Windows.Forms.Label authors;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label author1;
+        private System.Windows.Forms.Label author2;
+        private System.Windows.Forms.Label author3;
+        private System.Windows.Forms.Button buttonauthor1;
+        private System.Windows.Forms.Button buttonauthor2;
+        private System.Windows.Forms.Button buttonauthor3;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ Quiz 1-1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_1_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            author1.Text = " ";
+            author2.Text = " ";
+            author3.Text = " ";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void authors_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonauthor1_Click(object sender, EventArgs e)
+        {
+            author1.Text = "Margaret Mitchell";
+            author2.Text = " ";
+            author3.Text = " ";
+        }
+
+        private void buttonauthor2_Click(object sender, EventArgs e)
+        {
+            author2.Text = "Alex Haley";
+            author1.Text = " ";
+            author3.Text = " ";
+        }
+
+        private void buttonauthor3_Click(object sender, EventArgs e)
+        {
+            author3.Text = "Ayn Rand";
+            author1.Text = " ";
+            author2.Text = " ";
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
